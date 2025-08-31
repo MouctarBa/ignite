e5d2b8cd93aff7ce93127bfb502bb7065cc6f57c
MouctarBa <103814444+MouctarBa@users.noreply.github.com>
2025-08-31 05:09:21 -0600

    chore: add typed site settings factories

--- apps/cms/src/api/site-settings/routes/site-settings.ts
+++ apps/cms/src/api/site-settings/routes/site-settings.ts
@@ -2,6 +2,9 @@
  * site-settings router
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
+import type { UID } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::site-settings.site-settings');
+export default factories.createCoreRouter<'api::site-settings.site-settings'>(
+  'api::site-settings.site-settings' satisfies UID.ContentType
+);
